159c7673c4dab06d305c9f4204d39eae7413677f
Gleb Gorelov <glebfox@gmail.com>
2019-04-08 16:17:23 +0400

    CheckBoxGroupSimpleSample: IllegalArgumentException #31

--- modules/web/src/com/haulmont/sampler/web/ui/components/checkboxgroup/simple/CheckBoxGroupSimpleSample.java
+++ modules/web/src/com/haulmont/sampler/web/ui/components/checkboxgroup/simple/CheckBoxGroupSimpleSample.java
@@ -1,26 +1,10 @@
 package com.haulmont.sampler.web.ui.components.checkboxgroup.simple;
 
-import com.haulmont.cuba.core.global.Metadata;
-import com.haulmont.cuba.gui.model.InstanceContainer;
-import com.haulmont.cuba.gui.screen.*;
-import com.haulmont.sampler.entity.Customer;
-
-import javax.inject.Inject;
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
 
 @UiController("checkboxgroup-simple")
 @UiDescriptor("checkboxgroup-simple.xml")
 public class CheckBoxGroupSimpleSample extends ScreenFragment {
-
-    @Inject
-    private InstanceContainer<Customer> customerDc;
-    @Inject
-    private Metadata metadata;
-
-    @Subscribe
-    protected void onInit(InitEvent event) {
-        // InstanceContainer initialization. It is usually done automatically if the screen is
-        // inherited from StandardEditor and is used as an entity editor.
-        Customer customer = metadata.create(Customer.class);
-        customerDc.setItem(customer);
-    }
 }
--- modules/web/src/com/haulmont/sampler/web/ui/components/radiobuttongroup/simple/RadioButtonGroupSimpleSample.java
+++ modules/web/src/com/haulmont/sampler/web/ui/components/radiobuttongroup/simple/RadioButtonGroupSimpleSample.java
@@ -1,29 +1,10 @@
 package com.haulmont.sampler.web.ui.components.radiobuttongroup.simple;
 
-import com.haulmont.cuba.core.global.Metadata;
-import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.ScreenFragment;
-import com.haulmont.cuba.gui.screen.Subscribe;
 import com.haulmont.cuba.gui.screen.UiController;
 import com.haulmont.cuba.gui.screen.UiDescriptor;
-import com.haulmont.sampler.entity.Customer;
-
-import javax.inject.Inject;
 
 @UiController("radiobuttongroup-simple")
 @UiDescriptor("radiobuttongroup-simple.xml")
 public class RadioButtonGroupSimpleSample extends ScreenFragment {
-
-    @Inject
-    private InstanceContainer<Customer> customerDc;
-    @Inject
-    private Metadata metadata;
-
-    @Subscribe
-    protected void onInit(InitEvent event) {
-        // InstanceContainer initialization. It is usually done automatically if the screen is
-        // inherited from StandardEditor and is used as an entity editor.
-        Customer customer = metadata.create(Customer.class);
-        customerDc.setItem(customer);
-    }
 }
