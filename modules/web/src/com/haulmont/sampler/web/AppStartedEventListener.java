09734ec5a1bbc92a5f5af9d2cb6480c8752d8dc7
Gleb Gorelov <gorelov@haulmont.com>
2019-05-21 19:05:16 +0400

    Move onAppStarted from SamplerApp #32

---
+++ modules/web/src/com/haulmont/sampler/web/AppStartedEventListener.java
@@ -0,0 +1,25 @@
+package com.haulmont.sampler.web;
+
+import com.haulmont.cuba.core.sys.AppContext;
+import com.haulmont.cuba.security.auth.LoginPasswordCredentials;
+import com.haulmont.cuba.web.Connection;
+import com.haulmont.cuba.web.security.events.AppStartedEvent;
+import org.apache.commons.lang3.BooleanUtils;
+import org.springframework.context.ApplicationListener;
+
+import java.util.Locale;
+
+public class AppStartedEventListener implements ApplicationListener<AppStartedEvent> {
+
+    @Override
+    public void onApplicationEvent(AppStartedEvent event) {
+        if (!BooleanUtils.toBoolean(AppContext.getProperty("sampler.developerMode"))) {
+            Connection connection = event.getApp().getConnection();
+            if (!connection.isAuthenticated()) {
+                Locale locale = event.getApp().getLocale();
+                LoginPasswordCredentials credentials = new LoginPasswordCredentials("demo", "cuba123", locale);
+                connection.login(credentials);
+            }
+        }
+    }
+}
--- modules/web/src/com/haulmont/sampler/web/SamplerApp.java
+++ modules/web/src/com/haulmont/sampler/web/SamplerApp.java
@@ -1,16 +1,9 @@
 package com.haulmont.sampler.web;
 
 import com.haulmont.cuba.core.sys.AppContext;
-import com.haulmont.cuba.security.auth.LoginPasswordCredentials;
-import com.haulmont.cuba.security.global.LoginException;
-import com.haulmont.cuba.web.Connection;
 import com.haulmont.cuba.web.DefaultApp;
-import com.haulmont.cuba.web.security.events.AppStartedEvent;
 import com.haulmont.cuba.web.sys.RedirectHandler;
 import org.apache.commons.lang3.BooleanUtils;
-import org.springframework.context.event.EventListener;
-
-import java.util.Locale;
 
 public class SamplerApp extends DefaultApp {
 
@@ -25,17 +18,5 @@ public class SamplerApp extends DefaultApp {
 
     public RedirectHandler getRedirectHandler() {
         return redirectHandler;
-    }
-
-    @EventListener
-    protected void onAppStarted(AppStartedEvent event) throws LoginException {
-        if (!BooleanUtils.toBoolean(AppContext.getProperty("sampler.developerMode"))) {
-            Connection connection = event.getApp().getConnection();
-            if (!connection.isAuthenticated()) {
-                Locale locale = event.getApp().getLocale();
-                LoginPasswordCredentials credentials = new LoginPasswordCredentials("demo", "cuba123", locale);
-                connection.login(credentials);
-            }
-        }
     }
 }
