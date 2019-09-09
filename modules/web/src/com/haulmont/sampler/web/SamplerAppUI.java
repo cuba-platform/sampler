46c9887427388aab65c610769983d85c4e4d4602
Gleb Gorelov <gorelov@haulmont.com>
2019-09-09 11:38:16 +0400

    Support Google Analytics #48

--- modules/web/src/com/haulmont/sampler/web/SamplerAppUI.java
+++ modules/web/src/com/haulmont/sampler/web/SamplerAppUI.java
@@ -3,12 +3,16 @@ package com.haulmont.sampler.web;
 import com.haulmont.cuba.web.AppUI;
 import com.haulmont.sampler.web.config.SamplesAppConfig;
 import com.vaadin.server.VaadinRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.vaadin.googleanalytics.tracking.GoogleAnalyticsTracker;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 
 public class SamplerAppUI extends AppUI {
+
+    private static Logger log = LoggerFactory.getLogger(SamplerAppUI.class);
 
     @Inject
     private SamplesAppConfig samplesAppConfig;
@@ -28,6 +32,7 @@ public class SamplerAppUI extends AppUI {
 
     private void initGoogleAnalyticsTracker() {
         if (!samplesAppConfig.isGoogleAnalyticsTrackerEnabled()) {
+            log.info("GoogleAnalyticsTracker disabled");
             return;
         }
 
@@ -36,6 +41,8 @@ public class SamplerAppUI extends AppUI {
                 samplesAppConfig.getGoogleAnalyticsTrackerDomainName(),
                 "/sampler/");
         tracker.extend(this);
+
+        log.info("GoogleAnalyticsTracker enabled");
     }
 
     @Nullable
