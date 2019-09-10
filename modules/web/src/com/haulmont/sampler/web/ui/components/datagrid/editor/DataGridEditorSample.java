a7e901ba7828213d5039b5aebbb7cbfc23cbb73b
Gleb Gorelov <gorelov@haulmont.com>
2019-09-10 18:56:26 +0400

    Prevent DataGrid editor buffered mode changing if Editor is active

--- modules/web/src/com/haulmont/sampler/web/ui/components/datagrid/editor/DataGridEditorSample.java
+++ modules/web/src/com/haulmont/sampler/web/ui/components/datagrid/editor/DataGridEditorSample.java
@@ -1,5 +1,7 @@
 package com.haulmont.sampler.web.ui.components.datagrid.editor;
 
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.Notifications.NotificationType;
 import com.haulmont.cuba.gui.components.CheckBox;
 import com.haulmont.cuba.gui.components.DataGrid;
 import com.haulmont.cuba.gui.components.HasValue;
@@ -17,6 +19,9 @@ public class DataGridEditorSample extends ScreenFragment {
     @Inject
     private CheckBox editorBuffered;
 
+    @Inject
+    private Notifications notifications;
+
     @Subscribe
     protected void onInit(InitEvent event) {
         editorBuffered.setValue(customersDataGrid.isEditorBuffered());
@@ -24,6 +29,18 @@ public class DataGridEditorSample extends ScreenFragment {
 
     @Subscribe("editorBuffered")
     protected void onEditorBufferedValueChange(HasValue.ValueChangeEvent<Boolean> event) {
-        customersDataGrid.setEditorBuffered(Boolean.TRUE.equals(event.getValue()));
+        if (!event.isUserOriginated()) {
+            return;
+        }
+
+        if (customersDataGrid.isEditorActive()) {
+            notifications.create()
+                    .withCaption("Please close Editor before changing its mode")
+                    .withType(NotificationType.WARNING)
+                    .show();
+            editorBuffered.setValue(event.getPrevValue());
+        } else {
+            customersDataGrid.setEditorBuffered(Boolean.TRUE.equals(event.getValue()));
+        }
     }
 }
