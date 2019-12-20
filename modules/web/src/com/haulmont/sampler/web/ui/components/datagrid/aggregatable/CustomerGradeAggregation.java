6599b569151ed9ea030912066590ea75ab29e2d2
Gleb Gorelov <gorelov@haulmont.com>
2019-12-20 15:07:26 +0400

    Add sample with DataGrid aggregation #53

---
+++ modules/web/src/com/haulmont/sampler/web/ui/components/datagrid/aggregatable/CustomerGradeAggregation.java
@@ -0,0 +1,44 @@
+package com.haulmont.sampler.web.ui.components.datagrid.aggregatable;
+
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.data.aggregation.AggregationStrategy;
+import com.haulmont.sampler.entity.CustomerGrade;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.Collection;
+
+/**
+ * Calculate the most frequent customer grade
+ */
+public class CustomerGradeAggregation implements AggregationStrategy<CustomerGrade, String> {
+    @Override
+    public String aggregate(Collection<CustomerGrade> propertyValues) {
+        CustomerGrade mostFrequent = null;
+        long max = 0;
+        if (CollectionUtils.isNotEmpty(propertyValues)) {
+            for (CustomerGrade grade : CustomerGrade.values()) {
+                long current = propertyValues.stream()
+                        .filter(customerGrade -> customerGrade.equals(grade))
+                        .count();
+
+                if (current > max) {
+                    mostFrequent = grade;
+                    max = current;
+                }
+            }
+        }
+
+        if (mostFrequent != null) {
+            Messages messages = AppBeans.get(Messages.NAME);
+            return String.format("%s: %d/%d", messages.getMessage(mostFrequent), max, propertyValues.size());
+        }
+
+        return null;
+    }
+
+    @Override
+    public Class<String> getResultClass() {
+        return String.class;
+    }
+}
---
+++ modules/web/src/com/haulmont/sampler/web/ui/components/datagrid/aggregatable/DataGridAggregatableSample.java
@@ -0,0 +1,12 @@
+package com.haulmont.sampler.web.ui.components.datagrid.aggregatable;
+
+import com.haulmont.cuba.gui.screen.LoadDataBeforeShow;
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("datagrid-aggregatable")
+@UiDescriptor("datagrid-aggregatable.xml")
+@LoadDataBeforeShow
+public class DataGridAggregatableSample extends ScreenFragment {
+}
