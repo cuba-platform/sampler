11218bfdac876b25d1ab4cff0ce8ce8981fac9c0
Gleb Gorelov <gorelov@haulmont.com>
2019-12-20 19:19:41 +0400

    Add SplitPanel dockable and min/max pos demos #51

---
+++ modules/web/src/com/haulmont/sampler/web/ui/containers/splitpanel/dockable/SplitDockableSample.java
@@ -0,0 +1,10 @@
+package com.haulmont.sampler.web.ui.containers.splitpanel.dockable;
+
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("split-dockable")
+@UiDescriptor("split-dockable.xml")
+public class SplitDockableSample extends ScreenFragment {
+}
---
+++ modules/web/src/com/haulmont/sampler/web/ui/containers/splitpanel/position/SplitPositionSample.java
@@ -0,0 +1,10 @@
+package com.haulmont.sampler.web.ui.containers.splitpanel.position;
+
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("split-position")
+@UiDescriptor("split-position.xml")
+public class SplitPositionSample extends ScreenFragment {
+}
