4022fa868d464ab2403e676b4c1d685b3b18a30e
Gleb Gorelov <gorelov@haulmont.com>
2019-12-20 19:53:35 +0400

    Add sample with Slider #52

---
+++ modules/web/src/com/haulmont/sampler/web/ui/components/slider/SliderSimpleSample.java
@@ -0,0 +1,10 @@
+package com.haulmont.sampler.web.ui.components.slider;
+
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("slider-simple")
+@UiDescriptor("slider-simple.xml")
+public class SliderSimpleSample extends ScreenFragment {
+}
