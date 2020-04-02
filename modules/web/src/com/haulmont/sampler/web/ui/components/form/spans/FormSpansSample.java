fc772d06f1352ffd0436bdbee6775324b53748b4
Gleb Gorelov <gorelov@haulmont.com>
2020-04-02 15:27:47 +0400

    Form rowspan/colspan sample #60

---
+++ modules/web/src/com/haulmont/sampler/web/ui/components/form/spans/FormSpansSample.java
@@ -0,0 +1,10 @@
+package com.haulmont.sampler.web.ui.components.form.spans;
+
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("form-spans")
+@UiDescriptor("form-spans.xml")
+public class FormSpansSample extends ScreenFragment {
+}
