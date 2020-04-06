f9f5f4dc81561335c00ab7c731596833db2a3436
Gleb Gorelov <gorelov@haulmont.com>
2020-04-06 12:50:39 +0400

    DateField with autofill sample

---
+++ modules/web/src/com/haulmont/sampler/web/ui/components/datefield/autofill/AutofillDateFieldSample.java
@@ -0,0 +1,10 @@
+package com.haulmont.sampler.web.ui.components.datefield.autofill;
+
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("autofill-datefield")
+@UiDescriptor("autofill-datefield.xml")
+public class AutofillDateFieldSample extends ScreenFragment {
+}
