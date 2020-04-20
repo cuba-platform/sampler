02070b2ec4ea9dbd6b3b422a5acdce1ef92451f0
Gleb Gorelov <gorelov@haulmont.com>
2020-04-20 16:08:13 +0400

    Update DataGrid with Editor actions sample

--- modules/web/src/com/haulmont/sampler/web/ui/components/datagrid/editoractions/DataGridEditorActionsSample.java
+++ modules/web/src/com/haulmont/sampler/web/ui/components/datagrid/editoractions/DataGridEditorActionsSample.java
@@ -2,9 +2,7 @@ package com.haulmont.sampler.web.ui.components.datagrid.editoractions;
 
 import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.gui.Notifications;
-import com.haulmont.cuba.gui.actions.list.CreateAction;
-import com.haulmont.cuba.gui.actions.list.EditAction;
-import com.haulmont.cuba.gui.components.Actions;
+import com.haulmont.cuba.gui.components.Action;
 import com.haulmont.cuba.gui.components.DataGrid;
 import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
@@ -22,40 +20,32 @@ public class DataGridEditorActionsSample extends ScreenFragment {
     @Inject
     private CollectionContainer<Customer> customersDc;
     @Inject
-    private Actions actions;
-    @Inject
     private Notifications notifications;
     @Inject
     private Metadata metadata;
 
-    @SuppressWarnings("unchecked")
-    @Subscribe
-    protected void onInit(InitEvent event) {
-        CreateAction<Customer> createAction = (CreateAction<Customer>) actions.create(CreateAction.ID);
-        createAction.withHandler(actionPerformedEvent -> {
-            if (customersDataGrid.isEditorActive()) {
-                notifications.create()
-                        .withCaption("Close the editor before creating a new entity")
-                        .show();
-                return;
-            }
-            Customer newCustomer = metadata.create(Customer.class);
-            customersDc.getMutableItems().add(newCustomer);
-            customersDataGrid.edit(newCustomer);
-        });
-        customersDataGrid.addAction(createAction);
+    @Subscribe("customersDataGrid.create")
+    public void onCustomersDataGridCreate(Action.ActionPerformedEvent event) {
+        if (customersDataGrid.isEditorActive()) {
+            notifications.create()
+                    .withCaption("Close the editor before creating a new entity")
+                    .show();
+            return;
+        }
+        Customer newCustomer = metadata.create(Customer.class);
+        customersDc.getMutableItems().add(newCustomer);
+        customersDataGrid.edit(newCustomer);
+    }
 
-        EditAction<Customer> editAction = (EditAction<Customer>) actions.create(EditAction.ID);
-        editAction.withHandler(actionPerformedEvent -> {
-            Customer selected = customersDataGrid.getSingleSelected();
-            if (selected != null) {
-                customersDataGrid.edit(selected);
-            } else {
-                notifications.create()
-                        .withCaption("Item is not selected")
-                        .show();
-            }
-        });
-        customersDataGrid.addAction(editAction);
+    @Subscribe("customersDataGrid.edit")
+    public void onCustomersDataGridEdit(Action.ActionPerformedEvent event) {
+        Customer selected = customersDataGrid.getSingleSelected();
+        if (selected != null) {
+            customersDataGrid.edit(selected);
+        } else {
+            notifications.create()
+                    .withCaption("Item is not selected")
+                    .show();
+        }
     }
 }
